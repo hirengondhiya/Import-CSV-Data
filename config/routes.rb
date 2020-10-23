e8ffd8e7287bf67d0d9d4447c49bdd94421f05f6
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-10-23 14:26:45 +1100

    route to import csv

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: "people#index"
+  post 'people/import', to: 'people#import', as: 'import_people'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
