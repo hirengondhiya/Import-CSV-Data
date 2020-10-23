ef927dc3e6644ac63761ea3382b63e486cedeefe
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-10-23 16:38:30 +1100

    import action

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,10 +1,24 @@
 class PeopleController < ApplicationController
   def index
+    @people = Person.all
+    @import = People::Import.new
   end
 
   def import
-    count = ImportPeople.new(params[:file]).execute()
-    p "Imported #{count} users"
-    redirect_to root_path, notice: "Imported #{count} users"
+    @import = People::Import.new people_import_params
+    
+    if @import.process!
+      flash[:notice] = "Imported #{@import.imported_count} people successfully and #{@import.errors.count} failures."
+      redirect_to root_path
+    else
+      @people = Person.all
+      flash[:alert] = "Imported #{@import.imported_count} people successfully and #{@import.errors.count} failures."
+      render action: :index
+    end
+  end
+
+  private
+  def people_import_params
+    params.require(:people_import).permit(:file)
   end
 end
