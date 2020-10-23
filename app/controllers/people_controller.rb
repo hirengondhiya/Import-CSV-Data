d9ff82a7e0d0f0fe3c3d73703cb2eb6ece012ccc
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-10-23 22:43:58 +1100

    added people grid

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,10 @@
 class PeopleController < ApplicationController
   def index
-    @people = Person.all
+    @people_grid = initialize_grid(
+      Person, 
+      per_page: 10,
+      include:  [:locations, :affiliations, :weapon, :vehicle]
+      )
     @import = People::Import.new
   end
 
@@ -11,7 +15,11 @@ class PeopleController < ApplicationController
       flash[:notice] = "Imported #{@import.imported_count} people successfully and #{@import.errors.count} failures."
       redirect_to root_path
     else
-      @people = Person.all
+      @people_grid = initialize_grid(
+        Person, 
+        per_page: 10,
+        include:  [:locations, :affiliations, :weapon, :vehicle]
+      )
       flash[:alert] = "Imported #{@import.imported_count} people successfully and #{@import.errors.count} failures."
       render action: :index
     end
