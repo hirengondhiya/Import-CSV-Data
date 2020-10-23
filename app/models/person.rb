e128120d9967b0501a8675404b1f1ad4afa0feb0
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-10-23 13:13:04 +1100

    validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -7,4 +7,11 @@ class Person < ApplicationRecord
 
   has_one :person_weapon
   has_one :weapon, through: :person_weapon
+
+  validates :name, presence: true
+  validates :first_name, presence: true
+  validates :locations, presence: true
+  validates :species, presence: true
+  validates :gender, presence: true
+  validates :affiliations, presence: true
 end
