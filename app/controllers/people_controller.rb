fd2fcfcadb8d03489bb8ae44f9a0bb8c3804e6f8
Hiren Gondhiya <hirengondhiya@gmail.com>
2020-10-23 14:27:06 +1100

    action to import csv

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,10 @@
 class PeopleController < ApplicationController
   def index
   end
+
+  def import
+    count = ImportPeople.new(params[:file]).execute()
+    p "Imported #{count} users"
+    redirect_to root_path, notice: "Imported #{count} users"
+  end
 end
